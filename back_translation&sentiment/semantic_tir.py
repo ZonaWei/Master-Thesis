bb7f5aa3a66893687bfa5248172a3d0746a4a96b
Mingrou Wei <149289887+ZonaWei@users.noreply.github.com>
2025-11-26 04:43:08 +0100

    Update and rename sentiment_tir.py to semantic_tir.py

--- back_translation&sentiment/semantic_tir.py
+++ back_translation&sentiment/semantic_tir.py
@@ -76,7 +76,7 @@ for r in results:
     print(f"Match:    {r['semantic_match']}")
     print(f"Reason:   {r['explanation']}")
 
-with open("sentiment_result.txt", "w", encoding="utf-8") as f:
+with open("semantic_result.txt", "w", encoding="utf-8") as f:
      for r in results:
          f.write(f"[{r['index']}]\nOriginal: {r['tir_zero_shot']}\nBack: {r['back_translation_tir']}\nMatch: {r['semantic_match']}\nExplanation: {r['explanation']}\n\n")
 
